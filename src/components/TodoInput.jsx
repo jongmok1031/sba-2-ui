6ad7ab4f44ef105cd5407866c7a3872312de8175
jongmok1031 <jongmok1031@gmail.com>
2020-09-28 16:38:21 +0900

    16

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const TodoInput = () => {
+    // todo가 CRUD 대상(object) -> 속성이 됌
+    let [todo, setTodo] = useState(0)
     return <> 
     <h1>할일 등록</h1>
     <form action="">
