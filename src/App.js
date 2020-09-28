57df2af41d6a01caccca52a1dd292bedd6f3d958
jongmok1031 <jongmok1031@gmail.com>
2020-09-28 16:21:43 +0900

    16

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TodoInput from './components/TodoInput'
+
+const App = () =>{
+  return <div style = {{width: "100px", margin: "0 auto"}}>
+    <TodoInput/>
+  </div>
+}
+
+export default App;
+
