33fd800963bfc110e118a23f4356094937bc8a2c
jongmok1031 <jongmok1031@gmail.com>
2020-10-02 13:55:56 +0900

    src

--- src/App.js
+++ src/App.js
@@ -8,22 +8,32 @@ import {Provider} from 'react-redux'  //리액트 리덕스 연결해주는거
 import Alerts from './components/Alerts'
 import BasicElements from './components/Buttons'
 import Carousel from './components/Carousel'
-import CustomControls from './components/CustomControls'
+// import CustomControls from './components/CustomControls
+import Icons from './components/Icons' 
+import Inputs from './components/Inputs'
+import Landing from './components/Landing'
+
 const App = () =>{
   return <>
   <Provider store = {store}>
   
   <div style = {{width: "1000px", margin: "0 auto"}}>
     <Alerts/><br/><br/>
-    
+    <Landing/>
     <BasicElements/>
     <br/><br/>
     <TodoInput/>
     <TodoList/><br/>
     <br/>
     <br/><br/>
+    <Inputs/>
     <Carousel/><br/><br/>
-    <CustomControls/>
+    {/* <CustomControls/> */}
+    <Icons/>
+    <i className="ni ni-building"></i>
+    <i className="ni ni-building"></i>
+    <i className="ni ni-building"></i>
+    <i className="ni ni-building"></i>
   </div></Provider>
   </>
 }
